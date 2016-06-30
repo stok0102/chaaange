cf878b77a6ec9db7e6a2b20cf498d1488cc50125
Caleb Stokka and Chip Carnes <caleb+chip@>
2016-06-30 09:17:01 -0700

    returns the amount of pennies and nickels

--- spec/coin_combinations_spec.rb
+++ spec/coin_combinations_spec.rb
@@ -8,4 +8,7 @@ describe('Fixnum#coin_collect') do
   it('returns the amount of pennies needed') do
     expect(3.coin_collect()).to(eq('3 penny'))
   end
+  it('returns the amount of pennies and nickels') do
+    expect(8.coin_collect()).to(eq('1 nickel 3 penny'))
+  end
 end
